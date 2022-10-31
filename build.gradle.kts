ea3c174244904a7fb74e2372883eafc5efa24008
Alex Zenla <alex@fork.run>
2022-10-31 01:43:38 -0700

    Upgrade Kotlin and dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   application
 
-  kotlin("jvm") version "1.7.10"
-  kotlin("plugin.serialization") version "1.7.10"
+  kotlin("jvm") version "1.7.20"
+  kotlin("plugin.serialization") version "1.7.20"
 
   id("com.github.johnrengelman.shadow") version "7.1.2"
 }
@@ -27,9 +27,9 @@ java {
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-bom")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
   implementation("com.github.ajalt.clikt:clikt:3.5.0")
-  implementation("com.zaxxer:nuprocess:2.0.4")
+  implementation("com.zaxxer:nuprocess:2.0.5")
 }
 
 tasks.withType<KotlinCompile> {
