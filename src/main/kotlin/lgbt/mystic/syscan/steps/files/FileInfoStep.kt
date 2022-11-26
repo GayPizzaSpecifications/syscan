60ceba3d8e13f64b25b5700e20b52a51b0bd89f4
Alex Zenla <alex@fork.run>
2022-11-26 16:05:47 -0600

    Fix bug where directories would be scanned as files.

--- src/main/kotlin/lgbt/mystic/syscan/steps/files/FileInfoStep.kt
+++ src/main/kotlin/lgbt/mystic/syscan/steps/files/FileInfoStep.kt
@@ -3,6 +3,7 @@ package lgbt.mystic.syscan.steps.files
 import lgbt.mystic.syscan.analysis.AnalysisContext
 import lgbt.mystic.syscan.artifact.Artifact
 import lgbt.mystic.syscan.artifact.FileArtifact
+import lgbt.mystic.syscan.io.isDirectory
 import lgbt.mystic.syscan.io.isExecutable
 import lgbt.mystic.syscan.io.lastModifiedTime
 import lgbt.mystic.syscan.metadata.MetadataKeys
@@ -17,9 +18,13 @@ object FileInfoStep : FileAnalysisStep {
     if (artifact !is FileArtifact) {
       return
     }
-    artifact.metadata.set(this, FileMetadataKeys.ReadableFilePath, artifact.path)
+
+    if (!artifact.path.isDirectory()) {
+      artifact.metadata.set(this, FileMetadataKeys.ReadableFilePath, artifact.path)
+      artifact.metadata.set(this, FileMetadataKeys.ExecutableFileMarker, artifact.path.isExecutable())
+      artifact.metadata.set(this, FileMetadataKeys.LastModified, artifact.path.lastModifiedTime())
+    }
+
     artifact.metadata.set(this, FileMetadataKeys.VirtualFilePath, artifact.path)
-    artifact.metadata.set(this, FileMetadataKeys.ExecutableFileMarker, artifact.path.isExecutable())
-    artifact.metadata.set(this, FileMetadataKeys.LastModified, artifact.path.lastModifiedTime())
   }
 }
